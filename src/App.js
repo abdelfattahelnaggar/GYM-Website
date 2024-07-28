05ed715a06e9765c287f60634d4facbe062e28e6
Abdelfattah Elnaggar <elnaggara877@gmail.com>
2024-07-28 15:43:50 +0300

    initial commit to gym website

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import Feature from "./components/Feature";
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import Offer from "./components/Offer";
+import About from "./components/About";
+import Contact from "./components/Contact";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Header />
+      <Feature />
+      <Offer />
+      <About />
+      <Contact/>
+    </div>
+  );
+}
+
+export default App;
